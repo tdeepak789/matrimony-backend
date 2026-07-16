1cf450fce04c4fa8afe22075a3a77112c2795ee9
Tippavarjula Deepak <138246292+tdeepak789@users.noreply.github.com>
2026-07-17 00:21:07 +0530

    Add JwtBearer authentication to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyApp.services;
 using MyApp.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
